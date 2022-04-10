9b1011b71cac8b908b3db8ec00273a9948b281ff
Muhammad Soban <soban.se@gmail.com>
2022-04-10 15:17:47 +0500

    gallery added and other improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import PhotoSwipeLightbox from "photoswipe-lightbox"
+import PhotoSwipe from "photoswipe"
+
+document.addEventListener('turbo:load', () => {
+  // lightGallery(document.getElementById('lightgallery'), {
+  //   licenseKey: '0000-0000-000-0000',
+  //   speed: 500,
+  // });
+  const lightbox = new PhotoSwipeLightbox({
+    gallery: '#lightgallery',
+    children: 'a',
+    pswpModule: () => PhotoSwipe
+  });
+  lightbox.init();
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "photoswipe", to: "https://ga.jspm.io/npm:photoswipe@5.2.4/dist/photoswipe.esm.js"
+pin "photoswipe-lightbox", to: "https://ga.jspm.io/npm:photoswipe@5.2.4/dist/photoswipe-lightbox.esm.js"
