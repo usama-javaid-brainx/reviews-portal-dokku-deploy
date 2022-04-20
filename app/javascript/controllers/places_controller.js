5112f7c844281a78f0f954bf79a70101efb76c70
Muhammad Soban <soban.se@gmail.com>
2022-04-20 16:03:32 +0500

    show total reviews and just rating

--- app/javascript/controllers/places_controller.js
+++ app/javascript/controllers/places_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["field", "map", "name", "placeId", "latitude", "longitude", "address", "city", "state", "country", "reviews", "rating"]
+  static targets = ["field", "map", "name", "placeId", "latitude", "longitude", "address", "city", "state", "country", "reviews", "rating", "ratingCount"]
   static values = {
     latitude: String,
     longitude: String,
@@ -125,53 +125,10 @@ export default class extends Controller {
   }
 
   placeDetailsCallback(place, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      console.log(place)
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
       this.ratingTarget.innerHTML = place.rating
-      let reviews = ''
-      place.reviews.forEach((review) => {
-        reviews += `<div class="d-flex">
-            <div class="left">
-                        <span>
-                            <img src="${review.profile_photo_url}" class="profile-pict-img img-fluid" alt=""/>
-                        </span>
-            </div>
-            <div class="right">
-              <h4>
-                ${review.author_name}
-                <span class="gig-rating text-body-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" width="15" height="15">
-                                    <path
-                                      fill="currentColor"
-                                      d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"
-                                    ></path>
-                                </svg>
-                                ${review.rating}
-                            </span>
-              </h4>
-              <div class="review-description">
-                <p>
-                  ${review.text}
-                </p>
-              </div>
-              <span class="publish py-3 d-inline-block w-100">Published ${review.relative_time_description}</span>
-            </div>
-          </div>`
-      })
-
-      this.reviewsTarget.innerHTML = reviews
+      this.ratingCountTarget.innerHTML = place.user_ratings_total
+      this.ratingCountTarget.href = place.url
     }
   }
-
-  // get placeId() {
-  //   return this.data.get('placeId') || this.placeIdTarget.value
-  // }
-
-  // get latitude() {
-  //   return this.data.get('latitude') || this.latitudeTarget.value || 40.712775
-  // }
-  //
-  // get longitude() {
-  //   return this.data.get('longitude') || this.longitudeTarget.value || -74.005973
-  // }
 }
