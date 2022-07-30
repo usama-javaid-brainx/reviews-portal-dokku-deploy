29824c519ff47883d97b38cd20d798e45a623562
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-07-30 11:35:06 +0500

    Tag created on pressing enter

--- app/javascript/controllers/tags_controller.js
+++ app/javascript/controllers/tags_controller.js
@@ -3,10 +3,11 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["tagInput", "tags", "selectedTags"]
 
-  makeTag() {
+  makeTag(event) {
     let input_val = this.tagInputTarget.value.trim();
     let no_comma_val = input_val.replace(/,/g, "");
-    if (input_val.slice(-1) === "," && no_comma_val.length > 0) {
+    // if ((input_val.slice(-1) === "," && no_comma_val.length > 0) || event.key === "Enter" && no_comma_val.length > 0) {
+    if ((input_val.slice(-1) === "," && event.key === "Enter") && no_comma_val.length > 0) {
       var new_tag = compile_tag(no_comma_val);
       this.tagsTarget.appendChild(new_tag);
       this.selectedTagsTarget.value = this.selectedTagsTarget.value.concat(",", no_comma_val);
