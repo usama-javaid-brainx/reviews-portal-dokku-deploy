af61c710f9fda6933e4e1b9d18f7b3a75de0905a
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-08-02 16:56:43 +0500

    Add meal modal CRUD

--- app/javascript/controllers/meals_controller.js
+++ app/javascript/controllers/meals_controller.js
@@ -1,7 +1,7 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["favouriteMeals", "mealName", "mealNotes", "image", "edit", "mealId", "noMealDiv", "mealCard"]
+  static targets = ["favouriteMeals", "mealName" ,"mealNotes", "image", "edit", "mealId", "noMealDiv", "mealCard"]
 
   static get values() {
     return {editIcon: String, deleteIcon: String}
@@ -9,12 +9,13 @@ export default class extends Controller {
 
 
   saveChanges(event) {
-    debugger
-    if (event.currentTarget.id != "") {
+    if (event.currentTarget.id >= "1") {
       var favourite_dish = favourite_dishes[event.currentTarget.id - 1]
       favourite_dish["name"] = this.mealNameTarget.value
       favourite_dish["notes"] = this.mealNotesTarget.value
       favourite_dish["image"] = this.imageTarget.src
+      event.currentTarget.id = ""
+      this.updateCard(favourite_dish)
     } else {
       this.makeNewMeal()
       this.favouriteMealsTarget.classList.remove('d-none')
@@ -29,6 +30,11 @@ export default class extends Controller {
     favourite_dishes.push(favourite_dish)
     this.resetModal()
   }
+  updateCard(favDish){
+    document.getElementById(`image-${favDish["id"]}`).src = favDish["image"]
+    document.getElementById(`name-${favDish["id"]}`).innerText = favDish["name"]
+    document.getElementById(`notes-${favDish["id"]}`).innerText = favDish["notes"]
+  }
 
   editMeal(event) {
     let meal = favourite_dishes[event.currentTarget.id - 1]
@@ -40,20 +46,19 @@ export default class extends Controller {
   }
 
   deleteMeal() {
-    let meal_id  = this.mealCardTarget.getAttribute('data-id')
     this.mealCardTarget.remove()
-    if(favourite_dishes.length == 0) {
+    if(!this.hasMealCardTarget) {
       this.noMealDivTarget.classList.remove('d-none')
     }
   }
 
   makeNewMeal() {
-    let dish = `<li data-meals-target="mealCard" data-id="${favourite_dishes.length + 1}">
-       <img class='main_img' src='${this.imageTarget.src}'>
+    let dish = `<li data-meals-target="mealCard" id="${favourite_dishes.length + 1}">
+       <img id = "image-${favourite_dishes.length + 1}" class='main_img' src='${this.imageTarget.src}'>
       <div class='d-flex justify-content-between align-items-center w-100 ml_15'>
         <div class="text-left">
-          <h4>${this.mealNameTarget.value}</h4>
-          <p>${this.mealNotesTarget.value.substring(0, 20).trim()}...</p>
+          <h4 id = "name-${favourite_dishes.length + 1}"> ${this.mealNameTarget.value}</h4>
+          <p id = "notes-${favourite_dishes.length + 1}">${this.mealNotesTarget.value.substring(0, 20).trim()}...</p>
         </div>
         <span class="cursor-pointer">
             <span id="${favourite_dishes.length + 1}" data-action="click->meals#editMeal">
@@ -68,16 +73,10 @@ export default class extends Controller {
     this.favouriteMealsTarget.innerHTML = this.favouriteMealsTarget.innerHTML + dish
   }
 
-  resetModal() {
+  resetModal(event) {
     this.imageTarget.src = ""
     this.mealNameTarget.value = ""
     this.mealNotesTarget.value = ""
-  }
-
-  arrayRemove(mealId) {
-    return favourite_dishes.filter(function(ele){
-      return ele.id != mealId
-    });
   }
 }
 
