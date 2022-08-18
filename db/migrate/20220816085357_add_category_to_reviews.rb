e3fe46341dc2e4774a81c6aef47bb0e4f210f4b2
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-08-18 21:32:07 +0500

    Migrations removed

--- db/migrate/20220816085357_add_category_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToReviews < ActiveRecord::Migration[7.0]
-  def change
-    add_column :reviews, :category, :string
-  end
-end
--- db/migrate/20220818102108_remove_category_colunm_from_review.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCategoryColunmFromReview < ActiveRecord::Migration[7.0]
-  def up
-    remove_column :reviews, :category
-  end
-
-  def down
-    add_column :reviews, :category, :string
-  end
-end
