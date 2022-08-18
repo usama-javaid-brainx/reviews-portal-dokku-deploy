43536e336837252f59609b4b36307c6a77ff0ea9
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-08-18 21:40:59 +0500

    category reference updated

--- db/migrate/20220816105223_add_category_refrence_to_reviews.rb
+++ db/migrate/20220816105223_add_category_refrence_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefrenceToReviews < ActiveRecord::Migration[7.0]
   def change
-    add_reference :reviews, :categories, index: true
+    add_reference :reviews, :category, index: true
   end
 end
