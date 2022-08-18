5f2e51aed5d520208eb8183b931987bd0bbcb595
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-08-18 21:08:38 +0500

    irreversible migration adjusted

--- db/migrate/20220818102108_remove_category_colunm_from_review.rb
+++ db/migrate/20220818102108_remove_category_colunm_from_review.rb
@@ -1,5 +1,9 @@
 class RemoveCategoryColunmFromReview < ActiveRecord::Migration[7.0]
-  def change
+  def up
     remove_column :reviews, :category
+  end
+
+  def down
+    add_column :reviews, :category, :string
   end
 end
