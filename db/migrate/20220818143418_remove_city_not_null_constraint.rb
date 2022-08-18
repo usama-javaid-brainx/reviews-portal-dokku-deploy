c5dd855409c007df825c38e8762cba33a7e89be3
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-08-18 21:02:36 +0500

    migration changed

--- db/migrate/20220818143418_remove_city_not_null_constraint.rb
+++ db/migrate/20220818143418_remove_city_not_null_constraint.rb
@@ -1,5 +1,9 @@
 class RemoveCityNotNullConstraint < ActiveRecord::Migration[7.0]
-  def change
-    change_column :reviews, :city, :string, :null => true
+  def up
+    change_column :reviews, :city, :string, null: true
+  end
+
+  def down
+    change_column :reviews, :city, :string, null: false
   end
 end
