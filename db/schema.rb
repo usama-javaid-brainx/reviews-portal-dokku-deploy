2e558348267261eeb013545a4bac2534f2204e62
burhan-sabir-brainx <burhan.sabir@brainxtech.com>
2022-08-22 14:58:37 +0500

    toggle added for to try and Meals change to highlights

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_143418) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_083637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_143418) do
     t.boolean "shareable"
     t.text "images", default: [], array: true
     t.bigint "category_id"
+    t.boolean "to_try", default: false
     t.index ["category_id"], name: "index_reviews_on_category_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
