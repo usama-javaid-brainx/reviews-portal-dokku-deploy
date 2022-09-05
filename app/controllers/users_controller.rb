0633a027b4247f2b380901d0caf039aea76bc90d
Umer-iqbal <umer.iqbal@brainxtech.com>
2022-09-05 20:34:51 +0500

    implement edit page using devise registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,49 @@
-class ProfileController < ApplicationController
+class UsersController < ApplicationController
   def index
     reviews = review_filter(current_user.reviews.kept)
     @pagy, @reviews = pagy(reviews, items: 12)
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    debugger
+
+    if @user.update_with_password(params)
+      sign_in @user, :bypass => true
+
+      redirect_to root_path, :notice => "Your Password has been updated!"
+    else
+
+      flash[:alert] = @user.errors.full_messages.join("<br />")
+      render :edit
+
+    end
+
+    if @user.update(user_params)
+      debugger
+      # redirect_to user_profile
+    else
+      debugger
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
 
   private
+
+  def user_params
+    params[:last_name] = current_user.last_name
+    params[:email] = current_user.email
+    params[:encrypted_password] = current_user.encrypted_password
+    debugger
+    params.permit(:first_name, :avatar)
+  end
 
   def review_filter(reviews)
     params[:to_try] = 'all' unless params[:to_try].present?
