6f3ae6e742e0c2d32a6a0295e6f5ed14e1f62f45
Burhan <burhan.sabir@brainxtech.com>
2022-09-13 00:39:14 +0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_12_181730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,26 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_181730) do
     t.datetime "updated_at", null: false
     t.boolean "active", default: true
     t.integer "position"
-  end
-
-  create_table "ck_editor_images", force: :cascade do |t|
-    t.string "file"
-    t.integer "user_id"
-    t.integer "parent_id"
-    t.string "parent_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string "data_file_name", null: false
-    t.string "data_content_type"
-    t.integer "data_file_size"
-    t.string "data_fingerprint"
-    t.string "type", limit: 30
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "meals", force: :cascade do |t|
