39bb05f51fea12ba0359fee145427a8626da58f2
Burhan <burhan.sabir@brainxtech.com>
2022-09-14 17:29:45 +0500

    Reordering complete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,7 @@ class CategoriesController < ApplicationController
   end
 
   def update_categories_status
+    debugger
     if update_category?
       redirect_to root_path, status: :see_other, notice: "Category updated successfully!"
     else
@@ -13,7 +14,10 @@ class CategoriesController < ApplicationController
 
   def update_category?
     params[:categories].each do |category|
-      Category.find_by(name: category.first).update(active: category.second)
+      Category.find(category.first).update(active: category.second)
+    end
+    params[:positions].each do |position|
+      Category.find(position.first).update(position: position.second)
     end
   end
 end
--- app/javascript/controllers/category_reorder_controller.js
+++ app/javascript/controllers/category_reorder_controller.js
@@ -3,15 +3,35 @@ import Sortable from "sortablejs";
 
 export default class extends Controller {
 
+  static values = { position: Number }
+
   connect() {
     this.sortable = Sortable.create(this.element, {
-      onEnd: this.end.bind(this)
+      onEnd: this.changePosition.bind(this)
     })
   }
 
-  end(event) {
-    let id = event.item.dataset.id
-    let data = new FormData()
-    data.append("position", event.newIndex + 1)
+  changePosition(event) {
+    let oldIndex = event.oldIndex
+    let newIndex = event.newIndex
+    let dragableElements = event.target.children
+    if (oldIndex < newIndex) {
+      let i = oldIndex
+      while (i < newIndex) {
+        dragableElements[i].id = dragableElements[i].id - 1
+        i++
+      }
+      dragableElements[newIndex].id = parseInt(dragableElements[i - 1].id) + 1
+    } else {
+      let i = oldIndex
+      while (i > newIndex) {
+        dragableElements[i].id = parseInt(dragableElements[i].id) + 1
+        i--
+      }
+      dragableElements[newIndex].id = dragableElements[i + 1].id - 1
+    }
+    for(let i = 0; i < dragableElements.length; i++){
+      dragableElements[i].firstElementChild.value = i+1
+    }
   }
 }
