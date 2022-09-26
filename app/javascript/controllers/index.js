c847304e6e5f334abc2fe50e20aca429928bc53c
Umer-iqbal <umer.iqbal@brainxtech.com>
2022-09-26 17:31:34 +0500

    remove repetaiton of code

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -28,9 +28,6 @@ application.register("filter", FilterController)
 import FavouriteController from "./favourite_controller.js"
 application.register("favourite", FavouriteController)
 
-import UserValidationController from "./user_validation_controller.js"
-application.register("user-validation", UserValidationController)
-
 import PhotosController from "./photos_controller.js"
 application.register("photos", PhotosController)
 
