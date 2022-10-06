7f92dd9fe6b66f393edb773635bac34fa945a58d
Umer-iqbal <umer.iqbal@brainxtech.com>
2022-10-06 14:51:47 +0500

    remove unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :password, :avatar])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
   end
 
