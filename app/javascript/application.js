02bc0c71fa00bd25acdddb67cc9a969dfc29cc13
Burhan <burhan.sabir@brainxtech.com>
2022-10-06 18:31:15 +0500

    Style adjusted

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -63,6 +63,10 @@ $(document).ready(function () {
   $("#category_id").select2()
 });
 
+$(document).ready(function () {
+  $("#review_category_id").select2()
+});
+
 function callbackOnOpen1() {
   console.log(data);
 }
