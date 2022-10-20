42312ee8831f07b7970a7f42655bfcb009720bb8
Umer-iqbal <umer.iqbal@brainxtech.com>
2022-10-20 10:37:42 +0500

    validate user ph no just on update it should be uniq and can also be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,5 +33,5 @@ class User < ApplicationRecord
   #validation
   validates :first_name, :last_name, presence: true
   validates :email, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/, multiline: true }, presence: true, uniqueness: true
-  validates :phone_number, uniqueness: true
+  validates :phone_number, uniqueness: true, allow_blank: true, on: :update
 end
