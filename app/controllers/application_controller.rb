19fe85ea50bac2f5ec7066f65f02aa97a4a9eb6b
Burhan <burhan.sabir@brainxtech.com>
2022-10-26 17:36:56 +0500

    Code refactored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
     reviews = reviews.where('cuisine ilike any (array[?])', params[:cuisines_filter].split(',')) if params[:cuisines_filter].present?
     reviews = reviews.where('tags ilike any (array[?])', params[:tags_filter].split(',').map { |str| "%,#{str}%" }) if params[:tags_filter].present?
     reviews = if params[:score].present?
-                reviews.order(params[:score]== "recent"? "created_at desc" : "average_score #{params[:score]} NULLS LAST")
+                reviews.order(params[:score] == "recent" ? "created_at desc" : "average_score #{params[:score]} NULLS LAST")
               else
                 reviews.order("created_at desc")
               end
