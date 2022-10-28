7d9f63548d09a6dbeee14eafc2fe88e7db72c3cf
Burhan <burhan.sabir@brainxtech.com>
2022-10-28 19:03:47 +0500

    ability to create groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,12 @@
+class GroupsController < ApplicationController
+  def index
+    groups = current_user.groups.all
+    @pagy, @groups = pagy(groups, items: 12)
+  end
+
+  def show
+    @group = current_user.groups.find(params[:id])
+    reviews = current_user.groups.find(params[:id]).reviews
+    @pagy, @reviews = pagy(reviews, items: 12)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews, dependent: :destroy
+  has_many :groups, dependent: :destroy
   has_many :ck_editor_images, dependent: :destroy
   has_many :requests, dependent: :destroy
 
