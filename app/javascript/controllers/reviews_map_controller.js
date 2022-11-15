514a0141be10b2f050bb5332283364fed5359559
Burhan <burhan.sabir@brainxtech.com>
2022-11-15 19:29:05 +0500

    Marker added

--- app/javascript/controllers/reviews_map_controller.js
+++ app/javascript/controllers/reviews_map_controller.js
@@ -7,6 +7,7 @@ export default class extends Controller {
     this.reviewLocations = [];
     for (let i = 0; i < this.reviewDetailsTargets.length; i++) {
       let reviewLocation = {id: this.reviewDetailsTargets[i].getAttribute('id')};
+      reviewLocation["name"] = this.reviewDetailsTargets[i].getAttribute('data-name')
       reviewLocation["longitude"] = this.reviewDetailsTargets[i].getAttribute('data-longitude')
       reviewLocation["latitude"] = this.reviewDetailsTargets[i].getAttribute('data-latitude')
       this.reviewLocations.push(reviewLocation)
@@ -15,28 +16,29 @@ export default class extends Controller {
   }
 
   multiMarker() {
-    var infoWindow = new google.maps.InfoWindow();
+    const infoWindow = new google.maps.InfoWindow()
     var bounds = new google.maps.LatLngBounds();
-
     var map = new google.maps.Map(document.getElementById('reviewMap'), {
       zoom: 3,
       center: new google.maps.LatLng(52.2571543, 20.984522),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    let marker, i;
+    var marker, i;
     for (i = 0; i < this.reviewLocations.length; i++) {
       let latitude = parseFloat(this.reviewLocations[i]["latitude"]);
       let longitude = parseFloat(this.reviewLocations[i]["longitude"]);
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(latitude, longitude),
-        map: map
+        map: map,
+        title: this.reviewLocations[i]["name"],
       });
       bounds.extend(marker.position);
-      marker.addListener("click", () => {
-        infoWindow.close();
-        infoWindow.setContent(latitude);
-        infoWindow.open(map, marker);
-      });
+      google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
+        return function() {
+          infoWindow.setContent(marker.title);
+          infoWindow.open(map, marker);
+        }
+      })(marker, i));
     }
   }
 }
