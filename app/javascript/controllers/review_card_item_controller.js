1f41db13ca7a06c2c3729890d4976b26c73234c5
Burhan <burhan.sabir@brainxtech.com>
2022-11-16 19:16:26 +0500

    Code working

--- app/javascript/controllers/review_card_item_controller.js
+++ app/javascript/controllers/review_card_item_controller.js
@@ -3,16 +3,16 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["reviewCardDetails"]
 
-  creatMarker() {
-    var marker, i;
-    for (i = 0; i < this.reviewLocations.length; i++) {
-      let latitude = parseFloat(this.reviewLocations[i]["latitude"]);
-      let longitude = parseFloat(this.reviewLocations[i]["longitude"]);
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(latitude, longitude),
-        map: map,
-        title: this.reviewLocations[i]["name"],
-      });
-    }
-  }
+  // creatMarker() {
+  //   var marker, i;
+  //   for (i = 0; i < this.reviewLocations.length; i++) {
+  //     let latitude = parseFloat(this.reviewLocations[i]["latitude"]);
+  //     let longitude = parseFloat(this.reviewLocations[i]["longitude"]);
+  //     marker = new google.maps.Marker({
+  //       position: new google.maps.LatLng(latitude, longitude),
+  //       map: map,
+  //       title: this.reviewLocations[i]["name"],
+  //     });
+  //   }
+  // }
 }
--- app/javascript/controllers/reviews_map_controller.js
+++ app/javascript/controllers/reviews_map_controller.js
@@ -3,18 +3,9 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["reviewDetails"]
 
-  // connect() {
-  //   this.reviewLocations = [];
-  //   for (let i = 0; i < this.reviewDetailsTargets.length; i++) {
-  //     let reviewLocation = {id: this.reviewDetailsTargets[i].getAttribute('id')};
-  //     reviewLocation["name"] = this.reviewDetailsTargets[i].getAttribute('data-name')
-  //     reviewLocation["longitude"] = this.reviewDetailsTargets[i].getAttribute('data-longitude')
-  //     reviewLocation["latitude"] = this.reviewDetailsTargets[i].getAttribute('data-latitude')
-  //     this.reviewLocations.push(reviewLocation)
-  //   }
-  //   this.multiMarker()
-  //   this.allReviews()
-  // }
+  connect() {
+    this.allReviews()
+  }
 
   get reviewCardItemController() {
     return this.application.controllers.filter(controller => {
@@ -23,42 +14,61 @@ export default class extends Controller {
   }
 
   allReviews() {
-    this.allLocations = []
-    var allControllers = this.reviewCardItemController
-    for (let i = 0; i < allControllers.length; i++) {
-      let reviewLocation = {id: allControllers[i].element.getAttribute('id')}
-      reviewLocation["name"] = allControllers[i].element.getAttribute('data-name')
-      reviewLocation["longitude"] = allControllers[i].element.getAttribute('data-longitude')
-      reviewLocation["latitude"] = allControllers[i].element.getAttribute('data-latitude')
-      this.allLocations.push(reviewLocation)
-    }
-  }
-
-
-  multiMarker() {
+    let allControllers = this.reviewCardItemController
     const infoWindow = new google.maps.InfoWindow()
-    var bounds = new google.maps.LatLngBounds();
-    var map = new google.maps.Map(document.getElementById('reviewMap'), {
+    let bounds = new google.maps.LatLngBounds();
+    let marker, i;
+    let map = new google.maps.Map(document.getElementById('reviewMap'), {
       zoom: 3,
       center: new google.maps.LatLng(52.2571543, 20.984522),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    var marker, i;
-    for (i = 0; i < this.reviewLocations.length; i++) {
-      let latitude = parseFloat(this.reviewLocations[i]["latitude"]);
-      let longitude = parseFloat(this.reviewLocations[i]["longitude"]);
+    for (i = 0; i < allControllers.length; i++) {
+      let latitude = parseFloat(allControllers[i].element.getAttribute('data-latitude'));
+      let longitude = parseFloat(allControllers[i].element.getAttribute('data-longitude'));
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(latitude, longitude),
         map: map,
-        title: this.reviewLocations[i]["name"],
+        title: allControllers[i].element.getAttribute('data-name'),
       });
       bounds.extend(marker.position);
-      // google.maps.event.addListener(marker, 'mouseover', (function (marker, i) {
-      //   return function () {
-      //     infoWindow.setContent(marker.title);
-      //     infoWindow.open(map, marker);
-      //   }
-      // })(marker, i));
     }
   }
+
+
+  // for (let i = 0; i < allControllers.length; i++) {
+  //   let reviewLocation = {id: allControllers[i].element.getAttribute('id')}
+  //   reviewLocation["name"] = allControllers[i].element.getAttribute('data-name')
+  //   reviewLocation["longitude"] = allControllers[i].element.getAttribute('data-longitude')
+  //   reviewLocation["latitude"] = allControllers[i].element.getAttribute('data-latitude')
+  //   this.allLocations.push(reviewLocation)
+  // }
+
+
+  // multiMarker() {
+  // const infoWindow = new google.maps.InfoWindow()
+  // var bounds = new google.maps.LatLngBounds();
+  // var map = new google.maps.Map(document.getElementById('reviewMap'), {
+  //   zoom: 3,
+  //   center: new google.maps.LatLng(52.2571543, 20.984522),
+  //   mapTypeId: google.maps.MapTypeId.ROADMAP
+  // });
+  // var marker, i;
+  // for (i = 0; i < this.reviewLocations.length; i++) {
+  //   let latitude = parseFloat(this.reviewLocations[i]["latitude"]);
+  //   let longitude = parseFloat(this.reviewLocations[i]["longitude"]);
+  //   marker = new google.maps.Marker({
+  //     position: new google.maps.LatLng(latitude, longitude),
+  //     map: map,
+  //     title: this.reviewLocations[i]["name"],
+  //   });
+  //   bounds.extend(marker.position);
+  // google.maps.event.addListener(marker, 'mouseover', (function (marker, i) {
+  //   return function () {
+  //     infoWindow.setContent(marker.title);
+  //     infoWindow.open(map, marker);
+  //   }
+  // })(marker, i));
+  // }
+
 }
