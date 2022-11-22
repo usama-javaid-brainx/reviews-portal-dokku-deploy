f24e51735737d40710cf2e2270c8f20e9aa93659
Imran Akhtar <imran.akhtar@brainxtech.com>
2022-11-22 17:05:53 +0500

    pr resolved

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,7 @@ Rails.application.routes.draw do
     delete :delete_attachment, on: :member
     get :update_favourite
     get :get_score
-    collection do
-      post :index
-    end
+    post :index, on: :collection
   end
 
   post 'upload', to: 'file_uploads#upload'
