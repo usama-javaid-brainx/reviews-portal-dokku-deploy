c731264530e032e8b2af8a2f91384d44d96879e0
Muhammad Soban Akram <soban.se@gmail.com>
2022-12-13 00:38:17 +0500

    embed issue fixed

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -22,6 +22,18 @@ import Lightbox from 'bs5-lightbox';
       }, 500);
       event.preventDefault();
     });
+
+      document.querySelectorAll( 'oembed[url]' ).forEach( element => {
+      // Create the <a href="..." class="embedly-card"></a> element that Embedly uses
+      // to discover the media.
+      const anchor = document.createElement( 'a' );
+
+      anchor.setAttribute( 'href', element.getAttribute( 'url' ) );
+      anchor.className = 'embedly-card';
+
+      element.appendChild( anchor );
+    } );
+
   });
 
 })(jQuery);
