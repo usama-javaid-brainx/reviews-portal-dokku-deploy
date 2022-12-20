93dbf977923d9586b914eddb3eea0fed71b6b2d6
Usama Javaid <usama.javaid@brainxtech.com>
2022-12-20 19:23:14 +0500

    db schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_142811) do
     t.datetime "updated_at", null: false
     t.boolean "active", default: true
     t.integer "position"
-    t.bigint "user_id"
     t.string "sub_category_title"
     t.boolean "start_date"
     t.boolean "end_date"
@@ -73,7 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_142811) do
     t.boolean "foursquare_url"
     t.boolean "yelp_url"
     t.boolean "default_category", default: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "ck_editor_images", force: :cascade do |t|
@@ -123,17 +121,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_142811) do
   create_table "reviews", force: :cascade do |t|
     t.bigint "user_id"
     t.string "name", null: false
-    t.string "address"
     t.string "city"
-    t.string "state"
-    t.string "country"
-    t.string "place_id"
-    t.string "longitude"
-    t.string "latitude"
     t.string "cuisine"
     t.string "favorite_dish"
+    t.string "country"
     t.float "average_score"
     t.text "notes"
+    t.string "google_maps_link"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -143,16 +137,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_142811) do
     t.integer "status"
     t.boolean "favourite"
     t.boolean "shareable"
+    t.text "images", default: [], array: true
     t.bigint "category_id"
     t.boolean "to_try", default: false
     t.datetime "discarded_at"
-    t.text "images", default: [], array: true
     t.integer "parent_id"
     t.string "slug"
     t.date "start_date"
     t.date "end_date"
-    t.text "author"
-    t.text "platform"
+    t.string "author"
+    t.string "platform"
     t.string "url"
     t.string "google_url"
     t.string "foursquare_url"
