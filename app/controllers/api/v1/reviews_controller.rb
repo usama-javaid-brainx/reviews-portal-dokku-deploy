36bee25f63d84fdf5132f0eaf7ccc71f811761f3
Burhan <burhan.sabir@brainxtech.com>
2022-12-27 18:52:34 +0500

    Reviews with filters api

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -6,6 +6,10 @@ module Api
 
       def index
         reviews = Review.all
+        if (params[:filters].present? || params[:search].present? || params[:to_try].present? || params[:order].present? || params[:category_id].present?)
+          reviews = review_filter(reviews)
+        end
+        render json: reviews
       end
 
       def create
@@ -30,6 +34,23 @@ module Api
           message: msg
         }
       end
+
+      def review_filter(reviews)
+        reviews = reviews.where('state ilike any (array[?])', params[:search].split(' ')).or(reviews.where('state ilike any (array[?])', params[:search])).or(reviews.where('city ilike any (array[?])', params[:search].split(' '))).or(reviews.where('city ilike any (array[?])', params[:search])).or(reviews.where('country ilike any (array[?])', params[:search])).or(reviews.where('name ilike ?', "%#{params[:search]}%").or(reviews.where("cuisine ilike any (array[?])", params[:search])).or(reviews.where("tags ilike '%#{params[:search]}%'")).or(reviews.where("notes ilike '%#{params[:search]}%'"))) if params[:search].present?
+        reviews = params[:category_id] == 'all' ? reviews : reviews.where(category_id: params[:category_id]) if params[:category_id].present?
+        location = params[:filters][:location].map { |str| str.split(' . ') }.flatten if params[:filters][:location].present?
+        reviews = reviews.where('state ilike any (array[?])', location).or(reviews.where('city ilike any (array[?])', location)) if params[:filters][:location].present?
+        reviews = reviews.where('cuisine ilike any (array[?])', params[:filters][:cuisine]) if params[:filters][:cuisine].present?
+        reviews = reviews.where('tags ilike any (array[?])', params[:filters][:tag].map { |str| "%,#{str}%" }) if params[:filters][:tag].present?
+        reviews = reviews.where(to_try: params[:to_try]) if params[:to_try].present?
+        reviews = if params[:order].present?
+                    reviews.order(params[:order] == "recent" ? "created_at desc" : "average_score #{params[:order]} NULLS LAST")
+                  else
+                    reviews.order(Arel.sql("CASE WHEN date IS NOT NULL THEN date WHEN start_date IS NOT NULL THEN start_date ELSE created_at END"))
+                  end
+        reviews
+      end
+
     end
   end
 end
