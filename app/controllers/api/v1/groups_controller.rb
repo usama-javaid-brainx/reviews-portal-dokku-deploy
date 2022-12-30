cc50bc8f63cf80150310e0819f3d65b8b675c6da
Burhan <burhan.sabir@brainxtech.com>
2022-12-30 18:17:16 +0500

    Group documentation

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -2,61 +2,21 @@ module Api
   module V1
     class GroupsController < Api::V1::ApiController
 
-      api :GET, "Groups", "Get a list of all available groups"
+      api :GET, "groups", "Get a list of all available groups"
 
       example <<-EOS
         
       Status Codes with Response
       200: {
-    "reviews": [
+    "groups": [
         {
-            "id": 84,
-            "user_id": 1,
-            "name": "KFC Yum! Center",
-            "address": "1 Arena Plaza",
-            "city": "Louisville",
-            "state": "KY",
-            "country": "United States",
-            "place_id": "ChIJ8QWjWLxyaYgR5MYIpV71c0c",
-            "longitude": "-85.7542438",
-            "latitude": "38.25716789999999",
-            "cuisine": "Drama",
-            "favorite_dish": null,
-            "average_score": 5.0,
-            "notes": "<p>Very good</p>",
-            "date": "2022-11-15",
-            "created_at": "2022-11-17T11:02:57.829Z",
-            "updated_at": "2022-11-17T11:03:29.960Z",
-            "zip_code": "40202-1363",
-            "tags": ",hello,working",
-            "price_range": 4,
-            "status": null,
-            "favourite": null,
-            "shareable": true,
-            "category_id": 1,
-            "to_try": false,
-            "discarded_at": null,
-            "images": [
-                "https://cdn.filestackcontent.com/WCxWynf7QX2NJDhXgjqv",
-                "https://cdn.filestackcontent.com/2KIs4ADS4OrYQFOgUfQb"
-            ],
-            "parent_id": null,
-            "slug": "U9GSBhCiaArmMmrsgAFqFUedzBhQU51p84",
-            "start_date": null,
-            "end_date": null,
-            "author": null,
-            "platform": null,
-            "url": null,
-            "google_url": null,
-            "foursquare_url": "https://foursquare.com/v/antidote/4cab506a2f08236a745c8561",
-            "yelp_url": null
+            "id": 31,
+            "name": "Test",
+            "created_at": "2022-12-06T14:33:23.328Z",
+            "updated_at": "2022-12-06T14:33:23.328Z",
+            "user_id": 2,
+            "discarded_at": null
         }
-        ],
-        "meta": {
-            "current_page": 1,
-            "total_pages": 1
-        }
-    }
       EOS
 
       def index
@@ -88,7 +48,7 @@ module Api
         end
       end
 
-      api :DELETE, "Group", "Update a group"
+      api :DELETE, "group", "Delete a group"
       def destroy
         group = current_user.groups.find(params[:id])
         if group.discard
