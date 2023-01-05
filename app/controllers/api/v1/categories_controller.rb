c8a168012430ea8415da9d94dc219cf2e57f5ce7
Burhan <burhan.sabir@brainxtech.com>
2023-01-05 16:33:17 +0500

    Add response in sontroller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -9,28 +9,55 @@ module Api
       200: {
             "categories": [
               {
-                  "id": 1,
-                  "name": "Restaurants",
-                  "address": true,
-                  "google_places": true,
-                  "price": true,
-                  "cuisine": true,
-                  "created_at": "2022-12-20T21:04:12.260Z",
-                  "updated_at": "2022-12-20T21:04:12.260Z",
-                  "active": true,
-                  "position": null,
-                  "sub_category_title": null,
-                  "start_date": null,
-                  "end_date": null,
-                  "author": null,
-                  "platform": null,
-                  "url": null,
-                  "google_url": null,
-                  "foursquare_url": null,
-                  "yelp_url": null,
-                  "default_category": false,
-                  "icon_path": "http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--3745d7a2c3a1ea12155d58dc364affea8affc9b3/cutlery.svg"
-              }
+            "name": "Restaurants",
+            "address": true,
+            "icon": "http://localhost:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--3745d7a2c3a1ea12155d58dc364affea8affc9b3/cutlery.svg",
+            "google_places": true,
+            "price": true,
+            "cuisine": true,
+            "active": true,
+            "position": 1,
+            "sub_category_title": "Genres",
+            "start_date": null,
+            "end_date": null,
+            "author": null,
+            "platform": null,
+            "url": null,
+            "google_url": null,
+            "foursquare_url": null,
+            "yelp_url": null,
+            "created_at": "2022-08-23T10:16:26.562Z",
+            "updated_at": "2022-12-14T12:43:26.245Z",
+            "customized_fields": [
+                "Location",
+                "Name",
+                "Cuisines",
+                "Date",
+                "Url",
+                "Foursquare Url",
+                "Yelp Url",
+                "Score",
+                "Price",
+                "Description",
+                "Photos",
+                "Address",
+                "City",
+                "State",
+                "Zip Code",
+                "Country",
+                "Tags",
+                "Highlights"
+            ],
+            "sub_categories": [
+                {
+                    "id": 1,
+                    "category_id": 1,
+                    "name": "Action",
+                    "created_at": "2022-09-28T09:06:18.299Z",
+                    "updated_at": "2022-09-28T09:06:18.299Z"
+                }
+            ]
+        }
           ]
       }
       EOS
