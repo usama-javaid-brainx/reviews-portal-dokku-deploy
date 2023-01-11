2ce63e19017e50a10ca275e0ef5bf27a24996045
Usama Javaid <usama.javaid@brainxtech.com>
2023-01-11 21:32:21 +0500

    cuisine removed from serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
 
-  attributes :id, :user_id, :name, :address, :city, :state, :country, :place_id, :longitude, :latitude, :cuisine, :favorite_dish, :average_score, :notes, :date, :created_at, :updated_at, :zip_code, :tags, :price_range, :status, :favourite, :shareable, :category_id, :to_try, :discarded_at, :images, :parent_id, :slug, :start_date, :end_date, :author, :platform, :url, :google_url, :foursquare_url, :yelp_url
+  attributes :id, :user_id, :name, :address, :city, :state, :country, :place_id, :longitude, :latitude, :favorite_dish, :average_score, :notes, :date, :created_at, :updated_at, :zip_code, :tags, :price_range, :status, :favourite, :shareable, :category_id, :to_try, :discarded_at, :images, :parent_id, :slug, :start_date, :end_date, :author, :platform, :url, :google_url, :foursquare_url, :yelp_url
   has_many :meals
 
   def tags
