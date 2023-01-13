5f5a6b344b2d1184ef9070099314a255a734661f
Burhan <burhan.sabir@brainxtech.com>
2023-01-13 20:32:16 +0500

    Code refactored

--- db/migrate/20230113130250_change_defaultof_reviews_attributes.rb
+++ db/migrate/20230113130250_change_defaultof_reviews_attributes.rb
@@ -2,19 +2,17 @@ class ChangeDefaultofReviewsAttributes < ActiveRecord::Migration[7.0]
 
   def up
     columns = [:address, :city, :state, :country, :place_id, :longitude, :latitude, :url, :favorite_dish, :foursquare_url, :google_url, :yelp_url, :author, :platform, :notes]
-    default_value = ""
     columns.each do |column|
-      change_column_default :reviews, column, default_value
-      Review.where(column => nil).update_all(column => default_value)
+      change_column_default :reviews, column, ""
+      Review.where(column => nil).update_all(column => "")
     end
   end
 
   def down
     columns = [:address, :city, :state, :country, :place_id, :longitude, :latitude, :url, :favorite_dish, :foursquare_url, :google_url, :yelp_url, :author, :platform, :notes]
-    default_value = nil
     columns.each do |column|
-      change_column_default :reviews, column, default_value
-      Review.where(column => "").update_all(column => default_value)
+      change_column_default :reviews, column, nil
+      Review.where(column => "").update_all(column => nil)
     end
   end
 
